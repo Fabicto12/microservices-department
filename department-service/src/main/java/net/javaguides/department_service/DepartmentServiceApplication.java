afc13a11dc58ba618d90070851220c728fd5b259
Eduardo Vicente <eduardoartemis@gmail.com>
2026-04-08 10:31:03 -0500

    CORRECCION  de version de spring

--- department-service/src/main/java/net/javaguides/department_service/DepartmentServiceApplication.java
+++ department-service/src/main/java/net/javaguides/department_service/DepartmentServiceApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaClient
-
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
